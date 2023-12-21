2e0ef903a8538b6790bbde8eb6bdeae0111f79da
dashagarov <dashagarov@avito.ru>
2023-12-21 19:43:46 +0300

    change config to .env

--- location/cmd/location/main.go
+++ location/cmd/location/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"gitlab.com/hse-mts-go-dashagarov/go-taxi/location/internal/app"
 	"gitlab.com/hse-mts-go-dashagarov/go-taxi/pkg/houston/config"
+	"gitlab.com/hse-mts-go-dashagarov/go-taxi/pkg/houston/loggy"
 	"gitlab.com/hse-mts-go-dashagarov/go-taxi/pkg/houston/runner"
 	"log"
 	"time"
@@ -11,23 +12,19 @@ import (
 
 func main() {
 	ctx := context.Background()
-
-	err := config.ReadYAML()
-	if err != nil {
-		log.Fatalln("can't read config:", err)
-	}
+	loggy.InitDefault()
 
 	cfg := new(app.Config)
-	err = config.ParseYAML(cfg)
+	err := config.ReadAndParseEnv(cfg)
 	if err != nil {
-		log.Fatalln("can't parse config:", err)
+		log.Fatalln("can't read and parse config:", err)
 	}
 
 	a := app.NewApp(
 		cfg,
 		runner.NewRunnerV1(
-			config.String("app.name"),
-			config.String("jaeger.addr"),
+			config.String("APP_NAME"),
+			config.String("JAEGER_ADDR"),
 		),
 	)
 
--- location/internal/app/config.go
+++ location/internal/app/config.go
@@ -1,27 +1,27 @@
 package app
 
 type Config struct {
-	GRPC     `yaml:"grpc"`
-	HTTP     `yaml:"http"`
-	Swagger  `yaml:"swagger"`
-	Postgres `yaml:"postgres"`
+	GRPC     `yaml:"grpc" mapstructure:",squash"`
+	HTTP     `yaml:"http" mapstructure:",squash"`
+	Swagger  `yaml:"swagger" mapstructure:",squash"`
+	Postgres `yaml:"postgres" mapstructure:",squash"`
 }
 
 type GRPC struct {
-	Addr string `yaml:"addr"`
+	Addr string `yaml:"addr" mapstructure:"GRPC_ADDR"`
 }
 
 type HTTP struct {
-	Addr string `yaml:"addr"`
+	Addr string `yaml:"addr" mapstructure:"HTTP_ADDR"`
 }
 
 type Swagger struct {
-	Path string `yaml:"path"`
+	Path string `yaml:"path" mapstructure:"SWAGGER_PATH"`
 }
 
 type Postgres struct {
-	Host   string `yaml:"host"`
-	Port   string `yaml:"port"`
-	DBName string `yaml:"db_name"`
-	SSL    string `yaml:"ssl"`
+	Host   string `yaml:"host" mapstructure:"POSTGRES_HOST"`
+	Port   string `yaml:"port" mapstructure:"POSTGRES_PORT"`
+	DBName string `yaml:"db_name" mapstructure:"POSTGRES_DB_NAME"`
+	SSL    string `yaml:"ssl" mapstructure:"POSTGRES_SSL"`
 }
