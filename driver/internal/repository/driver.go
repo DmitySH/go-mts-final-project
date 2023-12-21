a797277c71e8a7337b5b85e5c347deabf5e28637
andrushchenko <andrushchenko@yandex-team.ru>
2023-12-21 23:51:28 +0300

    fix mongo query (UwU)

--- driver/internal/repository/driver.go
+++ driver/internal/repository/driver.go
@@ -1,15 +1,7 @@
 package repository
 
 import (
-	"context"
-	"fmt"
-
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
-
-	"gitlab.com/hse-mts-go-dashagarov/go-taxi/driver/internal/entity"
-	"gitlab.com/hse-mts-go-dashagarov/go-taxi/driver/internal/service"
-	"gitlab.com/hse-mts-go-dashagarov/go-taxi/pkg/houston/tracy"
 )
 
 const (
@@ -25,104 +17,4 @@ func NewDriverRepository(db *mongo.Client) *DriverRepository {
 	return &DriverRepository{
 		tripsColl: db.Database(databaseName).Collection(tripsCollectionName),
 	}
-}
-
-func createCursorError(ctx context.Context, err error) error {
-	err = fmt.Errorf("can't create cursor: %w", err)
-	tracy.RecordError(ctx, err)
-	return err
-}
-
-func executeError(ctx context.Context, err error) error {
-	err = fmt.Errorf("can't execute query: %w", err)
-	tracy.RecordError(ctx, err)
-	return err
-}
-
-func (d *DriverRepository) GetTrips(ctx context.Context) ([]entity.Trip, error) {
-	ctx, span := tracy.Start(ctx)
-	defer span.End()
-
-	cursor, err := d.tripsColl.Find(ctx, bson.M{})
-	if err != nil {
-		return nil, createCursorError(ctx, err)
-	}
-
-	defer cursor.Close(ctx)
-
-	var trips []entity.Trip
-	if err := cursor.All(ctx, trips); err != nil {
-		return nil, executeError(ctx, err)
-	}
-
-	return trips, nil
-}
-
-func (d *DriverRepository) GetTripByID(ctx context.Context, tripID string) (entity.Trip, error) {
-	ctx, span := tracy.Start(ctx)
-	defer span.End()
-
-	cursor, err := d.tripsColl.Find(ctx, bson.M{"id": tripID})
-	if err != nil {
-		return entity.Trip{}, createCursorError(ctx, err)
-	}
-
-	defer cursor.Close(ctx)
-
-	var trips []entity.Trip
-	if err := cursor.All(ctx, trips); err != nil {
-		return entity.Trip{}, executeError(ctx, err)
-	}
-
-	if len(trips) != 1 {
-		return entity.Trip{}, service.ErrEntityNotFound
-	}
-
-	return trips[0], nil
-}
-
-func (d *DriverRepository) UpdateTripStatus(ctx context.Context, tripID string, tripStatus entity.TripStatus) error {
-	ctx, span := tracy.Start(ctx)
-	defer span.End()
-
-	if _, err := d.tripsColl.UpdateOne(ctx, bson.M{"id": tripID}, bson.M{"$set": bson.M{"status": tripStatus}}); err != nil {
-		return executeError(ctx, err)
-	}
-
-	return nil
-}
-
-func (d *DriverRepository) UpdateTripDriver(ctx context.Context, tripID string, driverID string) error {
-	ctx, span := tracy.Start(ctx)
-	defer span.End()
-
-	if _, err := d.tripsColl.UpdateOne(ctx, bson.M{"id": tripID}, bson.M{"$set": bson.M{"driver": driverID}}); err != nil {
-		return executeError(ctx, err)
-	}
-
-	return nil
-}
-
-func (d *DriverRepository) CreateTrip(ctx context.Context, trip entity.Trip) error {
-	ctx, span := tracy.Start(ctx)
-	defer span.End()
-
-	cursor, err := d.tripsColl.Find(ctx, bson.M{"id": trip.Id})
-	if err != nil {
-		return createCursorError(ctx, err)
-	}
-
-	var trips []entity.Trip
-	if err := cursor.All(ctx, trips); err != nil {
-		return executeError(ctx, err)
-	}
-
-	defer cursor.Close(ctx)
-
-	if len(trips) != 0 {
-		return service.ErrEntityAlreadyExist
-	}
-
-	_, err = d.tripsColl.InsertOne(ctx, trip)
-	return err
 }
---
+++ driver/internal/repository/trips.go
@@ -0,0 +1,116 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"gitlab.com/hse-mts-go-dashagarov/go-taxi/driver/internal/entity"
+	"gitlab.com/hse-mts-go-dashagarov/go-taxi/driver/internal/service"
+	"gitlab.com/hse-mts-go-dashagarov/go-taxi/pkg/houston/tracy"
+)
+
+func createCursorError(ctx context.Context, err error) error {
+	err = fmt.Errorf("can't create cursor: %w", err)
+	tracy.RecordError(ctx, err)
+	return err
+}
+
+func executeError(ctx context.Context, err error) error {
+	err = fmt.Errorf("can't execute query: %w", err)
+	tracy.RecordError(ctx, err)
+	return err
+}
+
+func (d *DriverRepository) GetTrips(ctx context.Context) ([]entity.Trip, error) {
+	ctx, span := tracy.Start(ctx)
+	defer span.End()
+
+	cursor, err := d.tripsColl.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, createCursorError(ctx, err)
+	}
+
+	defer cursor.Close(ctx)
+
+	var trips []entity.Trip
+	if err := cursor.All(ctx, trips); err != nil {
+		return nil, executeError(ctx, err)
+	}
+
+	return trips, nil
+}
+
+func (d *DriverRepository) GetTripByID(ctx context.Context, tripID string) (entity.Trip, error) {
+	ctx, span := tracy.Start(ctx)
+	defer span.End()
+
+	cursor, err := d.tripsColl.Find(ctx, bson.M{"id": tripID})
+	if err != nil {
+		return entity.Trip{}, createCursorError(ctx, err)
+	}
+
+	defer cursor.Close(ctx)
+
+	var trips []entity.Trip
+	if err := cursor.All(ctx, trips); err != nil {
+		return entity.Trip{}, executeError(ctx, err)
+	}
+
+	if len(trips) != 1 {
+		return entity.Trip{}, service.ErrEntityNotFound
+	}
+
+	return trips[0], nil
+}
+
+func (d *DriverRepository) UpdateTripStatus(ctx context.Context, tripID string, tripStatus entity.TripStatus) error {
+	ctx, span := tracy.Start(ctx)
+	defer span.End()
+
+	result, err := d.tripsColl.UpdateOne(ctx, bson.M{"id": tripID}, bson.M{"$set": bson.M{"status": tripStatus}})
+	if err != nil {
+		return executeError(ctx, err)
+	}
+
+	if result.MatchedCount != 1 {
+		return service.ErrEntityNotFound
+	}
+
+	return nil
+}
+
+func (d *DriverRepository) UpdateTripDriver(ctx context.Context, tripID string, driverID string) error {
+	ctx, span := tracy.Start(ctx)
+	defer span.End()
+
+	result, err := d.tripsColl.UpdateOne(ctx, bson.M{"id": tripID, "driver_id": bson.M{"$exist": false}}, bson.M{"$set": bson.M{"driver_id": driverID}})
+	if err != nil {
+		return executeError(ctx, err)
+	}
+
+	if result.MatchedCount != 1 {
+		return service.ErrEntityNotFound
+	}
+
+	return nil
+}
+
+func (d *DriverRepository) CreateTrip(ctx context.Context, trip entity.Trip) error {
+	ctx, span := tracy.Start(ctx)
+	defer span.End()
+
+	opts := options.Update().SetUpsert(true)
+	result, err := d.tripsColl.UpdateOne(ctx, bson.M{"id": trip.Id}, trip, opts)
+	if err != nil {
+		return executeError(ctx, err)
+	}
+
+	if result.MatchedCount != 0 {
+		return service.ErrEntityAlreadyExist
+	}
+
+	return nil
+}
